f5191898f73277ebfe4465373d5d31ec768b771f
mrosarioch <mrosario-dev@clinicacruzjiminian.com>
2022-10-05 21:43:40 -0400

    first commit

---
+++ src/app/interfaces/Product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    _id?: string;
+    name: string,
+    asegura: string,
+    cedula: string,
+    poliza: string,
+    autoriza: string,
+    amount:Number
+    createdAt?: Date;
+}
+
